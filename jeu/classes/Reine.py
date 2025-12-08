8c39e51f58f3b5c83e83ff2efaca97b12dc0bef4
PernelleBlandin <pernelleblandin@gmail.com>
2025-12-08 18:15:08 +0100

    Maj Reine et Roi

--- jeu/classes/Reine.py
+++ jeu/classes/Reine.py
@@ -4,11 +4,19 @@ class Reine(Piece):
     nom: str
     deplacements: list[int]
     position: tuple
-    estNoir: bool
+    est_noir: bool
     mouvementsEffectues: int
     stringRep: str
 
-    def __init__(self, estNoir: bool, position: tuple, mouvementsEffectues: int) -> None:
+    def __init__(self, est_noir: bool, position: tuple, mouvementsEffectues: int) -> None:
         
-        super().__init__('Reine', estNoir, position, [(0, 1), (-1, 1), (-1, 0),  (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1)] if mouvementsEffectues == 0 else [(0, 1)], mouvementsEffectues = 0)
-        self.stringRep = '♕' if not self.estNoir else '♛'
+        super().__init__('Reine', est_noir, position, [(0, 1), (-1, 1), (-1, 0),  (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1)] if mouvementsEffectues == 0 else [(0, 1)], mouvementsEffectues = 0)
+        self.stringRep = '♕' if not self.est_noir else '♛'
+        
+    def __str__(self) -> str:
+        cote = 'Noir' if self.est_noir else 'Blanc'
+        return 'Type : Reine' \
+               ' - Position : ' + str(self.position) + \
+               ' - Côte : ' + cote + \
+               ' -- Value : ' + str(self.value) + \
+               ' -- Mouvements effectués : ' + str(self.mouvements_effectues)
--- jeu/classes/Roi.py
+++ jeu/classes/Roi.py
@@ -4,11 +4,20 @@ class Roi(Piece):
     nom: str
     deplacements: list[int]
     position: tuple
-    estNoir: bool
+    est_noir: bool
     mouvementsEffectues: int
     stringRep: str
 
-    def __init__(self, estNoir: bool, position: tuple, mouvementsEffectues: int) -> None:
+    def __init__(self, est_noir: bool, position: tuple, mouvementsEffectues: int) -> None:
         
-        super().__init__('Roi', estNoir, position, [(0, 1), (-1, 1), (-1, 0),  (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1)] if mouvementsEffectues == 0 else [(0, 1)], mouvementsEffectues = 0)
-        self.stringRep = '♔' if not self.estNoir else '♚'
+        #A ajuster, car la Reine peut se deplacer de plus d'une case par tour, meme si tjs dans la même diretion
+        super().__init__('Roi', est_noir, position, [(0, 1), (-1, 1), (-1, 0),  (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1)] if mouvementsEffectues == 0 else [(0, 1)], mouvementsEffectues = 0)
+        self.stringRep = '♔' if not self.est_noir else '♚'
+
+    def __str__(self) -> str:
+        cote = 'Noir' if self.est_noir else 'Blanc'
+        return 'Type : Roi' \
+               ' - Position : ' + str(self.position) + \
+               ' - Côte : ' + cote + \
+               ' -- Value : ' + str(self.value) + \
+               ' -- Mouvements effectués : ' + str(self.mouvements_effectues)
