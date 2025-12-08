2f6a62e2559478f420baea05890321e3022ed236
KuRika28 <erika.de-jesus-pinto-pereira@etu.univ-orleans.fr>
2025-12-08 17:33:48 +0100

    ajout docstring __eq__, changement de camelCase en snake_case

--- jeu/classes/Piece.py
+++ jeu/classes/Piece.py
@@ -2,24 +2,24 @@ class Piece:
     nom: str
     deplacements: list[int]
     position: tuple
-    estNoir: bool
-    mouvementsEffectues: int
+    est_noir: bool
+    mouvements_effectues: int
 
 
-    def __init__(self,nom: str, estNoir: bool, position: tuple, deplacements: list[int], mouvementsEffectues: int) -> None:
+    def __init__(self,nom: str, est_noir: bool, position: tuple, deplacements: list[int], mouvements_effectues: int) -> None:
         self.nom = nom
-        self.estNoir = estNoir
+        self.est_noir = est_noir
         self.position = position
         self.deplacements = []
-        self.mouvementsEffectues = 0
+        self.mouvements_effectues = 0
 
-    def __str__(self) -> str:
-        cote = 'Noir' if self.estN else 'Blanc'
-        return 'Type : Piece' \
-               ' - Position : ' + str(self.position) + \
-               ' - Côte : ' + cote + \
-               ' -- Value : ' + str(self.value) + \
-               ' -- Mouvements effectués : ' + str(self.mouvementsEffectues)
+    def getMouvementsPossibles(self) -> List[int]:  # type: ignore[empty-body]
+        """Retourne la liste des mouvements possibles de la pièce
+
+        Returns:
+            List[int]: la liste de tuples de mouvements possibles
+        """
+        return self.deplacements
 
     def estMouvementValide(self, position:tuple) -> bool:
         """Retourne vrai si la pièce peut effectuer le mouvement
@@ -34,9 +34,24 @@ class Piece:
             if self.position[0] + deplacement[0] == position[0] and self.position[1] + deplacement[1] == position[1]:
                 return True
         return False
-
+    
+    def __str__(self) -> str:
+        cote = 'Noir' if self.est_noir else 'Blanc'
+        return 'Type : Piece' \
+               ' - Position : ' + str(self.position) + \
+               ' - Côte : ' + cote + \
+               ' -- Value : ' + str(self.value) + \
+               ' -- Mouvements effectués : ' + str(self.mouvements_effectues)
 
     def __eq__(self, other: object) -> bool:
+        """Vérifie si deux pièces sont égales
+
+        Args:
+            other (object): la pièce à comparer
+
+        Returns:
+            bool: true si les pièces sont égales, false sinon
+        """
         if not isinstance(other, Piece):
             return NotImplemented
         if (
@@ -46,11 +61,3 @@ class Piece:
         ):
             return True
         return False
-
-    def getMouvementsPossibles(self) -> List[int]:  # type: ignore[empty-body]
-        """Retourne la liste des mouvements possibles de la pièce
-
-        Returns:
-            List[int]: la liste de tuples de mouvements possibles
-        """
-        return self.deplacements
