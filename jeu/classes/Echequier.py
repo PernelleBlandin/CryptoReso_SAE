6cf22365dee4c3cfbc0adf49c905eeac5f3d5cbb
Asma Hachelef <asma.hachelef1@gmail.com>
2025-12-08 17:56:10 +0100

    Début class echequier

--- jeu/classes/Echequier.py
+++ jeu/classes/Echequier.py
@@ -1,2 +1,40 @@
+from classes import *
+
+from classes.Pion import Pion
+
+PIECE= []
+
 class Echiquier():
+
     def __init__(self):
+        for x in range(8):
+            PIECE.append(Pion(self))#TODO REVOIR INITIALISATION
+        pass
+    
+    def initialiser_piece(self):
+        PIECE = []
+
+    def ajouter_piece(self, piece:Piece, pos: tuple):
+        PIECE.append((piece, pos))
+    
+    def deplacer(self, piece:Piece, pos: tuple):
+        pass
+
+    def get_piece(self, pos: tuple):
+        for p in PIECE:
+            if p[1] == pos:
+                return p[0]
+            
+    def est_vide(self, pos: tuple):
+        pass
+
+    def pos_valide(self, pos: tuple):
+        p
+    
+    def jouer(self):
+        pass
+
+        
+
+
+        
---
+++ jeu/classes/Pion.py
@@ -0,0 +1,3 @@
+class Pion (Piece):
+    def __init__(self, echequier; Echequier, pos: tuple):
+        pass
