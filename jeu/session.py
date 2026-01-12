c51bc9212386c71a7f2170b6493037e2ed4cb7a2
Asma Hachelef <asma.hachelef1@gmail.com>
2026-01-12 16:43:22 +0100

    modif session

--- jeu/session.py
+++ jeu/session.py
@@ -14,27 +14,30 @@ class Session:
 
 
    def mainSession(self):
-       print("Mise en place d'une nouvelle session...")
-       fini = False
-       while not fini:
-           if self.joueur1 == None:
+        
+        print("Mise en place d'une nouvelle session...")
+        fini = False
+        while not fini:
+            if self.joueur1 == None:
                self.file.write("Donner un nom pour le joueur Blanc : " + "\n")
                self.file.flush()
                pseudoBlanc = self.file.readline().strip()
                self.joueur1 = Joueur(pseudoBlanc, False)
+               
+            elif self.joueur2 == None:
                self.file.write("Donner un nom pour le joueur Noir : " + "\n")
                self.file.flush()
                pseudoNoir = self.file.readline().strip()
                self.joueur2 = Joueur(pseudoNoir, True)
 
 
-           self.file.write("Début de la partie..." + "\n")
-           self.file.flush()
-           line = self.file.readline().strip()
-           print("Commande reçue : " + line)
-           if line == "quit":
-               fini = True
-       print("Fermeture de la session actuelle...")
-       self.file.close()
-       self.socket.shutdown(socket.SHUT_RDWR)
-       self.socket.close()
+            self.file.write("Début de la partie..." + "\n")
+            self.file.flush()
+            line = self.file.readline().strip()
+            print("Commande reçue : " + line)
+            if line == "quit":
+                fini = True
+        print("Fermeture de la session actuelle...")
+        self.file.close()
+        self.socket.shutdown(socket.SHUT_RDWR)
+        self.socket.close()
