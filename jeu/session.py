f1da71007706720d9e029ad923549e422cc115e6
PernelleBlandin <pernelleblandin@gmail.com>
2026-01-12 17:02:31 +0100

    Liste des commandes du protocole dans session.py

--- jeu/session.py
+++ jeu/session.py
@@ -31,12 +31,50 @@ class Session:
                self.joueur2 = Joueur(pseudoNoir, True)
 
 
+            line = self.file.readline().strip()
             self.file.write("Début de la partie..." + "\n")
             self.file.flush()
-            line = self.file.readline().strip()
+
+
             print("Commande reçue : " + line)
-            if line == "quit":
+
+            if line == "register":
+                print("Pseudo du joueur Blanc : ")
+                # envoie au joueur 1 sur client1
+                
+
+                
+            if line == "connect":
+                print("A faire")
+                print("Votre pseudo : ")
+                print("Votre mot de passe : ")
+                
+
+            elif line == "play":
+                self.file.write()
+                self.file.flush()
+
+            elif line == "leave":
+                self.file.write("leave\n")
+                self.file.flush()
+                break
+
+            elif line == "quit":
                 fini = True
+
+            if line == "replay":
+                print("A faire")
+
+            if line == "new":
+                print("A faire")
+                # self.joueur1 = Joueur(pseudo_blanc, est_noir=False)
+                # self.joueur2 = Joueur(pseudo_noir, est_noir=True)
+
+            else:
+                self.file.write("err\n")
+                self.file.flush()
+
+
         print("Fermeture de la session actuelle...")
         self.file.close()
         self.socket.shutdown(socket.SHUT_RDWR)
