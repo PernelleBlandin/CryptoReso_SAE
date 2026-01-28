8fe7d1a9b58c12ee5a42c70dac205d35bca9ec45
MaelyssF <maelyss.fronton@etu.univ-orleans.fr>
2026-01-28 22:38:04 +0100

    Encodage et décodage fonctionnel dans test.py

--- jeu/test.py
+++ jeu/test.py
@@ -33,8 +33,35 @@
 # 
 # asyncio.run(main())
 
+from cryptography.hazmat.primitives.asymmetric import x25519
 from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-f = Fernet(key)
-token = f.encrypt(b"A really secret message. Not for prying eyes.")
-f.decrypt(token)
+import base64
+
+alice_private_key = x25519.X25519PrivateKey.generate()
+alice_public_key = alice_private_key.public_key()
+
+bob_private_key = x25519.X25519PrivateKey.generate()
+bob_public_key = bob_private_key.public_key()
+
+bob_shared_key = bob_private_key.exchange(alice_public_key)
+alice_shared_key = alice_private_key.exchange(bob_public_key)
+
+print(alice_shared_key == bob_shared_key)
+
+# bob
+codeur = Fernet(base64.urlsafe_b64encode(alice_shared_key))
+encrypted = codeur.encrypt("message à décoder".encode())
+
+print(encrypted)
+
+# alice
+decodeur = Fernet(base64.urlsafe_b64encode(bob_shared_key))
+decrypted = decodeur.decrypt(encrypted).decode()
+
+print(decrypted)
+
+# from cryptography.fernet import Fernet
+# key = Fernet.generate_key()
+# f = Fernet(key)
+# token = f.encrypt(b"A really secret message. Not for prying eyes.")
+# f.decrypt(token)
